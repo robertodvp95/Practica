2e1c0ece9655f23e50df5a7affeae2037b336afb
Roberto <Roberto@host.docker.internal>
2026-01-19 19:45:00 +0100

    -- Commit previo a la entrega de practica.

--- src/main/java/com/example/practica/PracticaApplication.java
+++ src/main/java/com/example/practica/PracticaApplication.java
@@ -3,7 +3,7 @@ package com.example.practica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Aplicacion principal
+// Clase principal de Ejecucion
 
 @SpringBootApplication
 public class PracticaApplication {
